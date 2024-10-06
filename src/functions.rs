1240bb40bde7e27e9b963247e7c9208f6609ae42
jd-apprentice <contacto@jonathan.com.ar>
2024-10-05 21:19:28 -0300

    chore: general improvements

---
+++ src/functions.rs
@@ -0,0 +1,20 @@
+/// # Panics
+/// Will panic if `SENTRY_DSN` is not set in the .env file
+#[inline]
+pub fn load_sentry() {
+    dotenvy::dotenv().unwrap_or_else(|_| {
+        panic!("Missing .env file");
+    });
+
+    let sentry_dns = env::var("SENTRY_DSN").unwrap_or_else(|_| {
+        panic!("Missing SENTRY_DSN environment variable");
+    });
+
+    let _guard = sentry::init((
+        sentry_dns,
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        },
+    ));
+}
